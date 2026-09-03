18aa42640d9aef944657b92172d946c81304ae04
KwaneleNtshangase <kwanelebc031@gmail.com>
2026-09-03 23:32:11 +0200

    feat(app): import gesture CSS in the app shell

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -14,6 +14,7 @@ import {
 import { usePathname } from "next/navigation";
 import { StatsPanel } from "@/components/StatsPanel";
 import { NothoTopBar } from "@/components/NothoTopBar";
+import "../gestures.css";
 
 function AppNavigation() {
   const { setRoute } = useNotho();
